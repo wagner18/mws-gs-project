a5be8d2286223eef9a5e50a3f6bb8a1af0f4747a
Wagner Borba <wagner18@gmail.com>
2018-07-19 14:30:00 -0500

    Switching to Gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+
+//import gulp from 'gulp';
+
+gulp.task('default', defaultTask);
+
+function defaultTask(done) {
+  // place code for your default task here
+  done();
+}
